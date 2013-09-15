5518b92fe71f842364b41f19f1794ce8e6d90b0e
Patrick Bartkus <patrickb.afg@gmail.com>
2013-09-15 13:45:51 -0400

    Enhanced members pages
    Changed members pages to check password on the server rathar than in
    the browser.

--- members/index.php
+++ members/index.php
@@ -2,72 +2,53 @@
 // Index - Members Login Page
 session_start(); 
 
-	$_SESSION['views']=$_SESSION['views']+1;
+if (isset($_POST["passwd"]))
+{
+	$sanitized_passwd =  strtolower(strip_tags(trim($_POST["passwd"]))) ;
+	// this is the main password checking "if" statement. Put whatever code you want here.
+	if (substr($sanitized_passwd,-3) == "afg")
+	{
+		$_SESSION['views']=$_SESSION['views']+1;
+		
+		if(isset($_SESSION['return'])){
+			header( 'Location: '.$_SESSION['return'] ) ;
+		}else {
+			// put the URI of the Main Members Page here
+			header( 'Location: http://www.ga-al-anon.org/members/memberswelcome.php' ) ;			
+		}
 
+	
+	}
+}
+// BTW, it's important that there is no space between the end of the PHP block and the DocType element
 ?><!doctype html>
 <html lang="en">
 <head>
-<meta charset="utf-8"> 
-
-
-<script language="JavaScript">
-
-function LogIn(){
-password="";
-return_URI="<?php echo $_SESSION['return']?>";
-password=document.form1.password1.value.toLowerCase();
-password=password.substring(password.length-3);
-if (password=="afg"){
-//	window.location='memberswelcome.html'
-	if (return_URI=="")
-	{
-// 			alert("no return_URI set");
-//		location.href=return_URI;
-		location.href="memberswelcome.php";
-	}
-	else
-	{
-		location.href=return_URI;
-
-	}
-}
-else{
-alert("Invalid log-in. Please enter your group name followed by afg into the textbox. Also, please make sure you click the button, do not press enter on your keyboard.");
-window.form1.password1.value="";
-}
-}
-function searchKeyPress(e)
-{
-if (window.event) 
-{e = window.event;}
-if (e.keyCode == 13)
-{
-LogIn();
-}
-}
-
-</script>
-
- 
+<meta charset="utf-8">  
 <title>GA Al-Anon/Alateen Members Area</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen"/>
 <body>
 
-<!-- Ask for Password -->
+<?php
+if (isset($_POST["passwd"])) {
+	echo "<h2>Sorry - Invalid log-in. Please enter your group name followed by afg into the textbox.</h2>" ;
+}
+
+?>
+<!-- Ask for Password - give a hint as to what to fill in here -->
 <H1>Al-Anon Members Area</H1>
 You have reached the Georgia Area Al-Anon / Alateen Members site. <br>
 This is a password protected site for Al-Anon and Alateen members.<br>
-Please enter the name of your home group followed by "afg" in the
-password box. Make sure you click the button. Do not press enter on
+Please enter the name of your home group followed by "afg" in the<br>
+password box. Make sure you click the button. Do not press enter on<br>
 your keyboard.<br>
 <br>
 
-<form name="form1">
+<form name="form1"  action="index.php" method="post">
 		<font face="Arial" color="#0000a0"><strong>Password:</strong></font>
 		<!--<input name="password1" type="password">-->
-		<input id="password1" onkeypress="searchKeyPress(event);" type="text">&nbsp; <input value="Click to Enter Site" onclick="LogIn()" type="button"> 
+		<input id="password1" name="passwd" type="text">
+		<input type="submit" value="Submit"> 
 </form>
 
 
